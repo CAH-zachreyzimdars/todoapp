a8f53ebfa09a3ac76b18df674ec4d2f9dc479b23
CAH-zachreyzimdars <zachrey.zimdars@cardinalhealth.com>
2021-04-14 13:28:40 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -11,6 +11,37 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+// import registerServiceWorker from './registerServiceWorker';
+// import { registerApplication, start } from 'single-spa';
+
+// registerApplication(
+//   'test', // Name of this single-spa application
+//   () => loadingFn(), // Our loading function
+//   () => true, // Our activity function
+// );
+
+// function loadingFn() {
+//   // Make sure the resolved value of the promise is an object!
+//   return System.import('test');
+// }
+
+// export function mount(props) {
+//   return Promise.resolve().then(() => {
+//     // Do framework UI rendering here
+//     console.log('mounted!');
+//   });
+// }
+
+// export function unmount(props) {
+//   return Promise.resolve().then(() => {
+//     // Do framework UI unrendering here
+//     console.log('unmounted!');
+//   });
+// }
+
+// start();
+// registerServiceWorker();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
